505e5614fa9b1aa8c69cfffbe02a3b2934b6537b
Victor Yang <victoryang@Victors-M1Pro-3.local>
2024-04-11 16:15:41 -0500

    init

---
+++ src/main/java/com/hongtayvy/beybladexapi/BeybladeXApiApplication.java
@@ -0,0 +1,13 @@
+package com.hongtayvy.beybladexapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeybladeXApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeybladeXApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongtayvy/beybladexapi/BeybladeXApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongtayvy.beybladexapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeybladeXApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
